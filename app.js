5b0c40be89b5afa5f357174028dc54ad7010fafe
rhakbari <developer-ivolve>
2022-06-14 10:21:53 +0500

    Pushed repo

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config')
+
+app.use(bodyParser.json());
+//Import Routes
+
+const postsRoute = require('./routes/posts')
+
+app.use('/posts', postsRoute)
+
+//routes
+
+app.get('/', (req, res) => {
+  res.send('We are on home')
+})
+
+//DB connection
+
+mongoose.connect(process.env.DB_CONNECTION, () => console.log('DB connected'))
+
+app.listen(3000)
---
+++ models/Post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const PostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+})
