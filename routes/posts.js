00f6002f2c83eecfcbcb2fef6a1925c28a0422a0
rhakbari <developer-ivolve>
2022-06-14 12:12:54 +0500

    update and delete done

--- routes/posts.js
+++ routes/posts.js
@@ -28,4 +28,37 @@ router.post('/', async (req, res) => {
   }
 })
 
+// get by id
+
+router.get('/:postId', async (req, res) => {
+  try {
+    console.log(req.params.postId)
+    const post = await Post.findById(req.params.postId)
+    res.json(post)
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
+//delete post
+
+router.delete('/:postId', async (req, res) => {
+  const deletePost = await Post.remove({ _id: req.params.postId })
+  res.json(deletePost)
+})
+
+//edit a post
+
+router.patch('/:postId', async (req, res) => {
+  try {
+    const updatedPost = await Post.updateOne(
+      { _id: req.params.postId },
+      { $set: { title: req.body.title } },
+    )
+    res.json(updatedPost)
+  } catch (err) {
+    res.json({ message: err })
+  }
+})
+
 module.exports = router
