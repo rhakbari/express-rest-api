f2983faddc19d1f47d87f40f1ac6abf0829e9c7d
rhakbari <developer-ivolve>
2022-06-14 11:37:08 +0500

    try catch done

--- routes/posts.js
+++ routes/posts.js
@@ -1,30 +1,31 @@
-const express = require('express');
-const router = express.Router();
-const Post = require('../models/Post');
+const express = require('express')
+const router = express.Router()
+const Post = require('../models/Post')
 
-
-router.get('/', (req, res) => {
-  res.send('We are on Posts')
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find()
+    res.json(posts)
+  } catch (err) {
+    res.json({ message: err })
+  }
 })
 
 router.get('/specific', (req, res) => {
   res.send('We are on a specific Post')
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const post = new Post({
     title: req.body.title,
     description: req.body.description,
   })
-  post.save()
-//   .exec()
-  .then(data => {
-    res.json(data)
-  })
-  .catch(err =>{
-
-    res.json({message: err})
-  })
+  try {
+    const savedPost = await post.save()
+    res.json(savedPost)
+  } catch (err) {
+    res.json({ message: err })
+  }
 })
 
 module.exports = router
